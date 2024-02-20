3e3ad2b60c9da0d2d3eadeb734653a8165df0421
Slava Richkov <v.v.richkov@yandex.ru>
2024-02-20 12:56:16 +0500

    feat: управление темой

--- src/contexts/theme/ThemeContext.tsx
+++ src/contexts/theme/ThemeContext.tsx
@@ -1,6 +1,9 @@
-import React, { createContext, useContext, useEffect, useState, } from 'react';
-import { Appearance } from 'react-native';
-import { saveSelectedThemeAsyncStore, getSelectedThemeAsyncStore } from '../../utils/asyncStoreFunctions';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import {Appearance} from 'react-native';
+import {
+  saveSelectedThemeAsyncStore,
+  getSelectedThemeAsyncStore,
+} from '../../utils/asyncStoreFunctions';
 
 /**
  * Контекст для работы с темой.
@@ -22,56 +25,56 @@ const ThemeContext = createContext();
  * @param {React.ReactNode} props.children - Дочерние компоненты, обертываемые ThemeContextProvider.
  * @returns {React.ReactNode} Компонент ThemeContextProvider.
  */
-export const ThemeContextProvider = ({ children }) => {
+export const ThemeContextProvider = ({children}) => {
+  const [theme, setTheme] = useState<string>('light');
+  const [colorText, setColorText] = useState({color: '#ffffff'});
+  const [colorTextModal, setColorTextModal] = useState({color: '#000000'});
+  const [backgroundColor, setBackgroundColor] = useState({
+    backgroundColor: '#c1c7cf',
+  });
 
-    const [theme, setTheme] = useState<string>('light');
-    const [colorText, setColorText] = useState({ color: '#ffffff' });
-    const [colorTextModal, setColorTextModal] = useState({ color: '#000000' });
-    const [backgroundColor, setBackgroundColor] = useState({ backgroundColor: '#c0c7cf' });
-
-    async function changeTheme(theme: 'light' | 'dark') {
-        if (theme === 'light' || theme === 'dark') {
-            await saveSelectedThemeAsyncStore(theme);
-            setTheme(theme);
-            if (theme === 'light') {
-                setBackgroundColor({ backgroundColor: 'gray' });
-                setColorTextModal({ color: 'rgba(0,0,0,1)' })
-            } else if (theme === 'dark') {
-                setBackgroundColor({ backgroundColor: '#333b42' }) // 292828
-                setColorTextModal({ color: 'rgba(255,255,255,1)' })
-            }
-        }
+  async function changeTheme(theme: 'light' | 'dark') {
+    if (theme === 'light' || theme === 'dark') {
+      await saveSelectedThemeAsyncStore(theme);
+      setTheme(theme);
+      if (theme === 'light') {
+        setBackgroundColor({backgroundColor: 'gray'});
+        setColorTextModal({color: 'rgba(0,0,0,1)'});
+      } else if (theme === 'dark') {
+        setBackgroundColor({backgroundColor: '#333b42'}); // 292828
+        setColorTextModal({color: 'rgba(255,255,255,1)'});
+      }
     }
+  }
 
-    function controlBackgrondColorTextColor(theme: string) {
-        if (theme === 'light') {
-            setBackgroundColor({ backgroundColor: 'gray' });
-            setColorTextModal({ color: 'rgba(0,0,0,1)' })
-        } else if (theme === 'dark') {
-            setBackgroundColor({ backgroundColor: '#333b42' }) // 292828
-            setColorTextModal({ color: 'rgba(255,255,255,1)' })
-        }
+  function controlBackgrondColorTextColor(theme: string) {
+    if (theme === 'light') {
+      setBackgroundColor({backgroundColor: '#c1c7cf'});
+      setColorTextModal({color: 'rgba(0,0,0,1)'});
+    } else if (theme === 'dark') {
+      setBackgroundColor({backgroundColor: '#333b42'}); // 292828
+      setColorTextModal({color: 'rgba(255,255,255,1)'});
     }
+  }
 
-    async function getTheme() {
-        const theme = await getSelectedThemeAsyncStore(); 
-        controlBackgrondColorTextColor(theme);
-        setTheme(theme);
-    }
+  async function getTheme() {
+    const theme = await getSelectedThemeAsyncStore();
+    controlBackgrondColorTextColor(theme);
+    setTheme(theme);
+  }
 
-    useEffect(() => {
-        getTheme()
-    }, [])
+  useEffect(() => {
+    getTheme();
+  }, []);
 
-    return (
-        <ThemeContext.Provider value={{ theme, colorText, colorTextModal, backgroundColor, changeTheme }}>
-            {children}
-        </ThemeContext.Provider>
-    )
-
-}
-
+  return (
+    <ThemeContext.Provider
+      value={{theme, colorText, colorTextModal, backgroundColor, changeTheme}}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 export const useTheme = () => {
-    return useContext(ThemeContext);
+  return useContext(ThemeContext);
 };
